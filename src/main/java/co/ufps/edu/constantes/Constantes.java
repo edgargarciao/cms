653c97a3da626e7b59cb29ed7f703a5fd2f118e6
ufpscms <40588558+ufpscms@users.noreply.github.com>
2018-06-27 15:38:45 -0500

    Update Constantes.java

--- src/main/java/co/ufps/edu/constantes/Constantes.java
+++ src/main/java/co/ufps/edu/constantes/Constantes.java
@@ -2,17 +2,25 @@ package co.ufps.edu.constantes;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import co.ufps.edu.util.FileUtil;
 
 public class Constantes {
+  
+  private static FileUtil fileUtil;
+  
+  public Constantes() {
+    fileUtil = new FileUtil();
+    this.RUTA = fileUtil.getProperties().getProperty("name");
+  }
 
   public static final String NOVEDAD = "Novedad";
   public static final String ACTIVIDAD = "Actividad";
   public static final String NOTICIA = "Noticia";
   public static final String SUBCATEGORIA = "Subcategoria";
   // For development
-  //public static final String RUTA = "ufps-graduados";
+    public static String RUTA = fileUtil.getProperties().getProperty("name");
   // For production
-  public static final String RUTA = "";
+  //public static final String RUTA = "";
   // For development
   //public static final String SERVER = "/";
   // For production
